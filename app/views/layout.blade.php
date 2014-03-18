ef67a83e9f3cbc6080f9d6c1153c7a49c02eafb7
officeboy <dsellinger@gmail.com>
2014-03-18 16:34:57 -0700

    search bar/nav bar formatting
    Didn't work, :(

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -8,12 +8,16 @@
 <body>
     <div class="container">
         <nav class="navbar navbar-default" role="navigation">
-            <div class="navbar-header">
-                <a href="{{ action('CustomersController@index') }}" class="navbar-brand">Customers</a>
-                <form action="{{ action('CustomersController@Search') }}" method="get" class="navbar-form navbar-left" role="search"> 
-                    <input type="text" class="form-control" name="q" placeholder="Search">
-                    <button type="Submit" class="btn btn-default"> Search</button>
-                </form>
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a href="{{ action('CustomersController@index') }}" class="navbar-brand">Customers</a>
+                    <form action="{{ action('CustomersController@Search') }}" method="get" class="navbar-form navbar-left" role="search"> 
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="q" placeholder="Search">
+                        </div>
+                        <button type="Submit" class="btn btn-default"> Search</button>
+                    </form>
+                </div>
             </div>
         </nav>
         @yield('content')
